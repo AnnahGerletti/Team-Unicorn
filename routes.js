be3a49970e9867b885bf29a5de489f9fbc850104
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-09-22 11:24:03 +1200

    harrison example hbsss

--- routes.js
+++ routes.js
@@ -1,9 +1,13 @@
 const express = require ('express')
 const router = express.Router()
-const data = require ('./data.json')
+// const data = require ('./data.json')
 
 router.get('/home', (req, res) => {
   res.send('home paged')
+})
+
+router.get('/test', (req, res) => {
+  res.render('test', {profiles: ['Harrison', 'Annah', 'Joe']})
 })
 
 module.exports = router
