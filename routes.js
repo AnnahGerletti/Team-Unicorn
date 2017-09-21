73fa079b77c6dab4fdab89508ee51a42dbc06706
eda013 <poukupenga@gmail.com>
2017-09-22 11:54:15 +1200

    update to merge master

--- routes.js
+++ routes.js
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 const express = require('express')
 const hbs = require('express-handlebars')
 const router = express.Router()
 const bodyParser = require('body-parser')
 
-const data = require('./data.json')
+// const data = require('./data')
 
 router.get('/', (req, res) => {
   res.send('Home')
@@ -13,14 +12,5 @@ router.get('/', (req, res) => {
 router.get('/battles/:id+id', (req, res) => {
   res.send('Battle page')
 })
-=======
-const express = require ('express')
-const router = express.Router()
-const data = require ('./data.json')
-
-router.get('/home', (req, res) => {
-  res.send('home paged')
-})
 
 module.exports = router
->>>>>>> eba0b4f6f7d6ca0dd887ba57c7234a32205f82c3
