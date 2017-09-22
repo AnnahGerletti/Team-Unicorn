9437405146f075173805563d2ed62c23b92389a1
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-09-22 12:23:18 +1200

    players button parcial, homepg view

--- routes.js
+++ routes.js
@@ -1,13 +1,13 @@
 const express = require ('express')
 const router = express.Router()
-// const data = require ('./data.json')
+const data = require ('./data.json')
+
+// router.get('/home', (req, res) => {
+//   res.send('home paged')
+// })
 
 router.get('/home', (req, res) => {
-  res.send('home paged')
-})
-
-router.get('/test', (req, res) => {
-  res.render('test', {profiles: ['Harrison', 'Annah', 'Joe']})
+  res.render('home', data)
 })
 
 module.exports = router
